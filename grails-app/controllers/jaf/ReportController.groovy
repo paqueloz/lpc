1ef7f65e463514deb14ef7c00520f590682b8f00
Pierre-Antoine Queloz <paqueloz@gmail.com>
2012-12-01 19:27:48 +0100

    add the SQL request for the export "applied for next year"

--- grails-app/controllers/jaf/ReportController.groovy
+++ grails-app/controllers/jaf/ReportController.groovy
@@ -271,9 +271,64 @@ class ReportController {
     def appliedForNextYear() 
     {
         if (params?.format && params.format != "html") {
+            // flush Hibernate before accessing the DB
+            assert sessionFactory != null
+            def hibSession = sessionFactory.getCurrentSession()
+            assert hibSession != null
+            hibSession.flush()
+    
+            def sql = Sql.newInstance(grailsApplication.config.dataSource.url,
+                    grailsApplication.config.dataSource.username,
+                    grailsApplication.config.dataSource.password,
+                    grailsApplication.config.dataSource.driverClassName)
+
             response.contentType = grailsApplication.config.grails.mime.types[params.format]
             response.setHeader("Content-disposition", "attachment; filename=addresses.${params.ext}")
-            def l = Person.findAllByAppliedForNextYear(false)
+            def l = []
+            sql.eachRow("""select 
+                p1.id,
+                p1.first_name, 
+                p1.last_name,
+                if(IFNULL(a.person_id,7)=7,concat(r.relationship,'(',p2.first_name, ' ',p2.last_name,')'),'Own address') AS TypAdr,
+                if(IFNULL(a.person_id,7)=7,a2.street1,a.street1) as Street1,
+                if(IFNULL(a.person_id,7)=7,a2.street2,a.street2) as Street2,
+                if(IFNULL(a.person_id,7)=7,a2.zip_code,a.zip_code) as Zip_code,
+                if(IFNULL(a.person_id,7)=7,a2.city,a.city) as City,
+                if(IFNULL(a.person_id,7)=7,reg2.short_key,reg.short_key) as Country,
+                (
+                select group_concat(concat(relationship,'(',p4.first_name, ' ',p4.last_name,') ')) from person_relation r4
+                left join
+                person p4 on r4.other_id = p4.id
+                where r4.person_id = p1.id
+                group by r4.person_id
+                ) as Relatives
+                
+                from person p1
+                left join person_relation r on p1.id=r.person_id and r.relationship='livesWith'
+                left join person p2 on p2.id=r.other_id
+                left join address a on p1.id = a.person_id
+                left join address a2 on r.other_id = a2.person_id
+                left join region reg on reg.id = a.country_id
+                left join region reg2 on reg2.id = a2.country_id
+                
+                where p1.applied_for_next_year>0;
+            """) {
+                // without the intermediate rec, export will not work
+                def rec = [
+                        id : it.id,
+                        first_name : it.first_name,
+                        last_name : it.last_name,
+                        TypAdr : it.TypAdr,
+                        Street1 : it.Street1,
+                        Street2 : it.Street2,
+                        Zip_code : it.Zip_code,
+                        City : it.City,
+                        Country : it.Country,
+                        Relatives : it.Relatives,
+                    ]
+                l << rec
+            }
+            log.info("Result:${l.size()}")
             exportService.export(params.format, response.outputStream, l, [:], [:])
         }
     }
